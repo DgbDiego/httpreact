3eeff9d917cfca4d80c0354403376075ac08bb29
DgbDiego <dgb.diego@gmail.com>
2025-12-10 16:05:50 -0300

    feat: adicionar suporte ao json-server e configurar scripts para execução simultânea

--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <code>
+            <p>Sistema de servidor rodando</p>
+            <p>Clique <a className='App-link' target="_blank" href='http://localhost:3001/products'>aqui</a> para visualizar os dados de db.json</p>
+          </code>
       </header>
     </div>
   );
